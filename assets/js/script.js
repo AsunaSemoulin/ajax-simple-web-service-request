74bb7143a67470ec274ce81a55c37a48c460ad6b
AsunaSemoulin <89163001+AsunaSemoulin@users.noreply.github.com>
2021-10-06 09:50:13 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let quote = document.getElementById("quote");
-
+let author = document.getElementById("author");
 fetch("https://thatsthespir.it/api")
     .then(response => response.json())
-    .then(data => quote.innerHTML = data.quote)
+    .then(data => {
+        quote.innerHTML = data.quote;
+        author.innerHTML = data.author;
+    })
     .catch(error => quote.innerHTML = error);
